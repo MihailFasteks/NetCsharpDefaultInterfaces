b8058c265d05906117bde91792844c00b00fe765
fasta <fastakovskiy.mihail04@gmail.com>
2024-11-15 00:05:36 +0200

    Добавьте файлы проекта.

---
+++ NetCsharpDefaultInterfaces/Classes.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace NetCsharpDefaultInterfaces
+{
+    internal class Book : IComparable, ICloneable
+    {
+        public string name { get; set; }
+        public string author { get; set; }
+        public int year { get; set; }
+        public string describing { get; set; }
+        public int pageNumber { get; set; }
+
+        public Book()
+        {
+            name = "";
+            author = "";
+            year = 0;
+            describing = "";
+            pageNumber = 0;
+        }
+        public Book(string n, string a, string desc, int y, int pn)
+        {
+            name = n;
+            author = a;
+            describing = desc;
+            year = y;
+            pageNumber = pn;
+        }
+        public void Show()
+        {
+            Console.WriteLine("\n{0}   {1}   {2}   {3}   {4}", name, author, year, pageNumber, describing);
+        }
+        public override string ToString()
+        {
+            return string.Format("\n{0}, {1}, {2}", name, author, describing, year, pageNumber);
+        }
+        public void Input()
+        {
+            Console.WriteLine("\nВведите название книги: ");
+            name = Console.ReadLine();
+            Console.WriteLine("\nВведите имя и фамилию автора: ");
+            author = Console.ReadLine();
+            Console.WriteLine("\nВведите описание книги: ");
+            describing = Console.ReadLine();
+            Console.WriteLine("\nВведите год издания: ");
+            year = int.Parse(Console.ReadLine());
+            Console.WriteLine("\nВведите кол-во страниц: ");
+            pageNumber = int.Parse(Console.ReadLine());
+        }
+        public int CompareTo(object obj)
+        {
+            if (obj is Book)
+                return name.CompareTo((obj as Book).name);
+
+            throw new NotImplementedException();
+        }
+        public class SortByName : IComparer
+        {
+            int IComparer.Compare(object obj1, object obj2)
+            {
+                if (obj1 is Book && obj2 is Book)
+                    return (obj1 as Book).name.CompareTo((obj2 as Book).name);
+
+                throw new NotImplementedException();
+            }
+        }
+        public class SortByAuthor : IComparer
+        {
+            int IComparer.Compare(object obj1, object obj2)
+            {
+                if (obj1 is Book && obj2 is Book)
+                    return (obj1 as Book).author.CompareTo((obj2 as Book).author);
+
+                throw new NotImplementedException();
+            }
+        }
+        public class SortByYear : IComparer
+        {
+            int IComparer.Compare(object obj1, object obj2)
+            {
+                if (obj1 is Book && obj2 is Book)
+                    return (obj1 as Book).year.CompareTo((obj2 as Book).year);
+
+                throw new NotImplementedException();
+            }
+        }
+        public class SortByPages : IComparer
+        {
+            int IComparer.Compare(object obj1, object obj2)
+            {
+                if (obj1 is Book && obj2 is Book)
+                    return (obj1 as Book).pageNumber.CompareTo((obj2 as Book).pageNumber);
+
+                throw new NotImplementedException();
+            }
+        }
+        public object Clone()
+        {
+            return new Book(name, author, describing, year, pageNumber);
+        }
+
+    }
+    class Library : IEnumerable, IEnumerator
+    {
+        Book[] ar;
+        int curpos = -1;
+        public Library(int len)
+        {
+            ar = new Book[len];
+            for (int i = 0; i < len; i++)
+            {
+                ar[i] = new Book();
+            }
+        }
+
+        public Library() : this(1) { }
+
+        public Library(Book[] books)
+        {
+            ar = new Book[books.Length];
+            for (int i = 0; i < books.Length; i++)
+            {
+                ar[i] = new Book(books[i].name, books[i].author, books[i].describing, books[i].year, books[i].pageNumber);
+            }
+        }
+
+        public void InputClub()
+        {
+            for (int i = 0; i < ar.Length; i++)
+                ar[i].Input();
+        }
+
+        public void ShowClubs()
+        {
+            for (int i = 0; i < ar.Length; i++)
+                ar[i].Show();
+        }
+
+
+        public IEnumerator GetEnumerator()
+        {
+            Console.WriteLine("\nВыполняется метод GetEnumerator");
+            // возвращается ссылка на объект класса, реализующего перечислитель
+            return this;
+        }
+
+        // реализация перечислителя
+        #region enumerator
+
+        //Устанавливает перечислитель в его начальное положение, т. е. перед первым элементом коллекции
+        public void Reset()
+        {
+            Console.WriteLine("\nВыполняется метод Reset");
+            curpos = -1;
+        }
+        public object Current // Получает текущий элемент в коллекции
+        {
+            get
+            {
+                Console.WriteLine("\nВыполняется свойство Current");
+                return ar[curpos];
+            }
+        }
+
+        // Перемещает перечислитель к следующему элементу коллекции
+        public bool MoveNext()
+        {
+            Console.WriteLine("\nВыполняется метод MoveNext");
+            if (curpos < ar.Length - 1)
+            {
+                curpos++;
+                return true;
+            }
+            else
+            {
+                Reset();
+                return false;
+            }
+
+        }
+        #endregion enumerator
+    }
+}
---
+++ NetCsharpDefaultInterfaces/Program.cs
@@ -0,0 +1,73 @@
+﻿// See https://aka.ms/new-console-template for more information
+using NetCsharpDefaultInterfaces;
+using System;
+
+
+Book[] book = new Book[4];
+Console.WriteLine("Неупорядоченный массив:");
+book[0] = new Book("Paper towns", "John Green", "dfvfdvdfvdf", 2008, 120);
+book[1] = new Book("Borrow life", "Erich Remarque", "dfgvdfvdfv", 1966, 146);
+book[2] = new Book("I Robot", "Isaac Azimov", "dfgdfgdfdfg", 1967, 245);
+book[3] = new Book("1984", "George Orwell", "sfsrfsdsd", 1989, 251);
+
+foreach (Book temp in book)
+    temp.Show();
+Console.WriteLine();
+Array.Sort(book);
+Console.WriteLine("\nУпорядоченный массив:");
+foreach (Book temp in book)
+    temp.Show();
+Console.WriteLine();
+Array.Sort(book, new Book.SortByName());
+Console.WriteLine("\nМассив, упорядоченный по имени:");
+foreach (Book temp in book)
+    temp.Show();
+Console.WriteLine();
+Array.Sort(book, new Book.SortByAuthor());
+Console.WriteLine("\nМассив, упорядоченный по автору:");
+foreach (Book temp in book)
+    temp.Show();
+Console.WriteLine();
+Array.Sort(book, new Book.SortByYear());
+Console.WriteLine("\nМассив, упорядоченный по году:");
+foreach (Book temp in book)
+    temp.Show();
+Console.WriteLine();
+Array.Sort(book, new Book.SortByPages());
+Console.WriteLine("\nМассив, упорядоченный по кол-ву страниц:");
+foreach (Book temp in book)
+    temp.Show();
+Console.WriteLine();
+
+
+
+Book b1 = new Book();
+Console.WriteLine(b1);
+Book b2 = b1; 
+b2.name = "Athlantida";
+b2.author = "Marius Blake";
+b2.describing = "fertrecet";
+b2.year = 2013;
+b2.pageNumber = 384;
+Console.WriteLine(b1.ToString());
+Func(b1);
+Console.WriteLine(b1.ToString());
+
+static void Func(ICloneable cloneable)
+{
+    object o = cloneable.Clone(); 
+    Console.WriteLine(o.ToString());
+    Book b1 = o as Book;
+    b1.name = "Azazel";
+    b1.author = "Boris Acunin";
+    b1.describing = "fertrecet";
+    b1.year = 2015;
+    b1.pageNumber = 220;
+    Console.WriteLine(o.ToString());
+}
+
+Library lb = new(book);
+foreach (Book temp in lb)
+    temp.Show();
+foreach (Book temp in lb)
+    temp.Show();
